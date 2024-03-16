e5369bf20a8805785df6685f1b169a3da3b7cd12
Gilles Hoarau <unicolored@gmail.com>
2024-03-16 10:06:33 +0100

    Add strapi and pgadmin

--- play.sh
+++ play.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-ansible-playbook -e @group_vars/vault.yml --vault-password-file .vault_passwd ./play-teamcity.yml -i ./hosts.ini
+ansible-playbook -e @group_vars/vault.yml --vault-password-file .vault_passwd ./play.yml -i ./hosts.ini
